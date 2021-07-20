47ccaf5f85b9c11a5d8a5151e36c2bc4047d7c1e
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-07-20 10:26:32 -0700

    create employees with instant variable that has one to many relationship

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,42 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(
+  first_name: "Khurram",
+  last_name: "Virani",
+  hourly_rate: 50
+)
+
+@store1.employees.create(
+  first_name: "Jack",
+  last_name: "Jang",
+  hourly_rate: 12
+)
+
+@store1.employees.create(
+  first_name: "Ken",
+  last_name: "Kang",
+  hourly_rate: 14
+)
+
+@store2.employees.create(
+  first_name: "Bill",
+  last_name: "Lion",
+  hourly_rate: 50
+)
+
+@store2.employees.create(
+  first_name: "Spider",
+  last_name: "Man",
+  hourly_rate: 32
+)
+
+@store2.employees.create(
+  first_name: "Kelly",
+  last_name: "Kim",
+  hourly_rate: 10
+)
+
+puts @store1.employees.count
+puts @store2.employees.count
